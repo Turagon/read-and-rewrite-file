1494d15def1abb69df14a2bc4300b0d6045caf65
Turagon <52981127+Turagon@users.noreply.github.com>
2020-03-26 22:05:20 -0700

    Add files via upload

---
+++ readtest.py
@@ -0,0 +1,46 @@
+import os
+
+
+
+def read_file(filename) :
+	lines = []	
+	with open(filename, "r", encoding = "utf-8-sig") as f :
+		for line in f :
+			lines.append(line.strip())
+	return lines
+
+def convert(raw_content) :
+	new = []
+	person = None
+	for line in raw_content :
+		if line == "Allen" :
+			person = "Allen"
+			continue
+		elif line == "Tom" :
+			person = "Tom"
+			continue
+		if person :
+			new.append(person+": "+line)
+	return new
+
+def write_file(filename, content) :
+	with open(filename, "w", encoding = "utf-8") as f :
+		for line in content :
+			f.write(line+"\n")
+		
+
+def main() :
+	lines = read_file("input.txt")
+	new = convert(lines)
+	write_file("output.txt", new)
+
+
+main()
+with open("output.txt", "r", encoding = "utf-8") as f :
+	for line in f :
+		print(line)
+
+
+
+
+
